743c10a297e6417125753e7c54bced2ca1b3b9b6
Christian Kranz <christian.kranz@adesso.de>
2022-06-10 09:02:31 +0200

    added content to c testfile

--- testfiles/test.c
+++ testfiles/test.c
@@ -1,5 +1,6 @@
 char* MSG = "HELLO WORLD";
 char* SCREENBUFFERPOINTER = (char*)0x4000;
+int SCREENOFFSET = 0;
 
 
 void MAIN()
@@ -9,5 +10,13 @@ void MAIN()
 
 void PRINT(char* MSG)
 {
+    char attribute = 0x07;
 
+    while(*MSG != 0)
+    {
+        SCREENBUFFERPOINTER[SCREENOFFSET] = *MSG;
+        SCREENBUFFERPOINTER[SCREENOFFSET + 1] = attribute;
+        SCREENOFFSET += 2;
+        MSG++;
+    }
 }
